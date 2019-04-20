1c800a31492876de3151eb23746c41858fcc8720
Richard Kimber-Bell <rickykimberbell@gmail.com>
2019-04-20 19:26:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   var arr=[]
   var n
   for(n=0; n<musicians.length; n++) {
-    arr.push(`${musicians[n]} plays the ${instruments[n]}`)
+    arr.push(`${musicians[n]} plays ${instruments[n]}`)
   }
   return arr
 }
